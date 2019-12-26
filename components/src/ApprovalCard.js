3f43cfeb7f88638176422c44b0115726df41becb
VitaliiLynnyk <linnik2101@gmail.com>
2019-12-26 14:30:31 +0200

    add ApprovalCard

---
+++ components/src/ApprovalCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const ApprovalCard = props => {
+  const { children } = props;
+
+  return (
+    <div className="ui cards">
+      <div className="content">{children}</div>
+      <div class="extra content">
+        <div class="ui two buttons">
+          <div class="ui basic green button">Approve</div>
+          <div class="ui basic red button">Decline</div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- components/src/index.js
+++ components/src/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 
+import { ApprovalCard } from './ApprovalCard';
 import { CommentDetail } from './CommentDetail';
 
 const App = () => {
   return (
-    <div className="ui container comments">
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author={faker.name.firstName()}
-        timeAgo={faker.date.weekday()}
-        content={faker.lorem.sentence()}
-      />
+    <div
+      className="ui container comments centered grid"
+      style={{ marginTop: '50px' }}
+    >
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author={faker.name.firstName()}
+          timeAgo={faker.date.weekday()}
+          content={faker.lorem.sentence()}
+        />
+      </ApprovalCard>
     </div>
   );
 };

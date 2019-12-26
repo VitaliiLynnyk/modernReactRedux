255fb60de0877690f26e1af5f912090a95f17b51
VitaliiLynnyk <linnik2101@gmail.com>
2019-12-26 14:03:57 +0200

    add faker data

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-import faker from 'faker';
-
 export const CommentDetail = props => {
+  const { avatar, author, timeAgo, content } = props;
+
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
-          Sam
+          {author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
+          <span className="date">{timeAgo}</span>
         </div>
-        <div className="text">Nice blog post !</div>
+        <div className="text">{content}</div>
       </div>
     </div>
   );
--- components/src/index.js
+++ components/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 import { CommentDetail } from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author={faker.name.firstName()}
+        timeAgo={faker.date.weekday()}
+        content={faker.lorem.sentence()}
+      />
     </div>
   );
 };

400c57f01825cbff303e2818a863cb7c5c5e3fab
VitaliiLynnyk <linnik2101@gmail.com>
2019-12-27 18:14:53 +0200

    try to get curr position

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-  return <SeasonDisplay />;
-};
+class App extends Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+
+    return <div></div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
